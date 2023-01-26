a88aa5d36833e411efeb386929968356eef73e59
Eniola Somoye <110903578+EniolaSomoye-16@users.noreply.github.com>
2023-01-26 11:44:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const chatBody = document.querySelector(".chat-body");
+const txtInput = document.querySelector("#txtInput");
+const send = document.querySelector(".send");
+
+send.addEventListener("click", () => renderUserMessage());
+
+txtInput.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    renderUserMessage();
+  }
+});
+
+const renderUserMessage = () => {
+  const userInput = txtInput.value;
+  renderMessageEle(userInput, "user");
+  txtInput.value = "";
+  setTimeout(() => {
+    renderChatbotResponse(userInput);
+    setScrollPosition();
+  }, 600);
+};
+
+const renderChatbotResponse = (userInput) => {
+  const res = getChatbotResponse(userInput);
+  renderMessageEle(res);
+};
+
+const renderMessageEle = (txt, type) => {
+  let className = "user-message";
+  if (type !== "user") {
+    className = "chatbot-message";
+  }
+  const messageEle = document.createElement("div");
+  const txtNode = document.createTextNode(txt);
+  messageEle.classList.add(className);
+  messageEle.append(txtNode);
+  chatBody.append(messageEle);
+};
+
+const getChatbotResponse = (userInput) => {
+  return responseObj[userInput] == undefined
+    ? "Please try something else"
+    : responseObj[userInput];
+};
+
+const setScrollPosition = () => {
+  if (chatBody.scrollHeight > 0) {
+    chatBody.scrollTop = chatBody.scrollHeight;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+// my
+const bar = document.getElementById('bar');
+
+const close = document.getElementById('close');
+
+const nav = document.getElementById('navbar')
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active')
+  })
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
+}
