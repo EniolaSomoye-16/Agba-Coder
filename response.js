a0ae442fc1a767986c7e29796a0b5d746935dec3
Eniola Somoye <110903578+EniolaSomoye-16@users.noreply.github.com>
2023-01-26 10:40:59 +0100

    Add files via upload

---
+++ response.js
@@ -0,0 +1,13 @@
+
+
+const sent = "i need help with my project"
+const responseObj = {
+  hello: "Hey ! How are you doing ?, what can i do for you",
+  hey: "Hey! What's Up",
+  hi: "Hey! there",
+  thankyou: "Hey! there",
+  "i need help with my project": "what is the name of your project?",
+  "you are the best": "thank you",
+  "what is your name": "my name is RAINBOT",  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
